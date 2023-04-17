c608acb8b07271086c7efa029fa3ec1a37af0e7b
Cloudy_Kumori <837055906@qq.com>
2023-04-17 22:45:40 +0800

    Fixed some bugs that caused accident exit or loop.

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,8 +1,7 @@
 import random
 
 number = random.randint(1, 100)
-user_input = input('''Let's play a guess number game! 
-
+user_input = input('''Hi, I'm Cloudy. Let's play a guess number game! 
 Please input a integer number between 1 and 100, and I'll tell you if it's bigger or smaller. :''')
 while not user_input.isdigit():
     user_input = input('Invalid input, please try again! :')
@@ -13,16 +12,30 @@ else:
 while user_input != number:
     if user_input in range(1, 101):
         if user_input > number:
-            user_input = int(input('The number you input is big, try again! :'))
+            user_input = input('The number you input is big, try again! :')
+            while not user_input.isdigit():
+                user_input = input('Invalid input, please try again! :')
+                continue
+            user_input=int(user_input)
             continue
         elif user_input < number:
-            user_input = int(input('The number you input is small, try again! :'))
+            user_input = input('The number you input is small, try again! :')
+            while not user_input.isdigit():
+                user_input = input('Invalid input, please try again! :')
+                continue
+            user_input=int(user_input)
             continue
         elif user_input == number:
             print('Congratulations! You are right, the number is', number)
+            input('Press any key to exit.')
             break
     else:
-        print('Oops! The number you entered is not between 1-100, try again! :')
-        break
+        user_input=input('Oops! The number you entered is not between 1-100, try again! :')
+        while not user_input.isdigit():
+            user_input = input('Invalid input, please try again! :')
+            continue
+        user_input=int(user_input)
+        continue
 else:
     print('Congratulations! You are right, the number is', number, '!')
+    input('Press any key to exit.')
